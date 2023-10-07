970f6995dd853ca1399031918fe7d06251663e53
csefikatalin <csefi.katalin@gmail.com>
2023-10-07 17:59:53 +0200

    orai fszf

--- Megjelenit.js
+++ Megjelenit.js
@@ -4,18 +4,24 @@ class Megjelenit {
     #list = [];
     constructor(list, szuloElem) {
         this.#list = list;
-        szuloElem.append('<table class="table table-bordered table-striped ">');
+        szuloElem.append(
+            '<table class="table table-bordered table-striped  ">'
+        );
         this.tablaElem = szuloElem.children("table");
-        console.log(this.tablaElem);
+
         this.tablazatbaIr();
         $(window).on("torles", (event) => {
             console.log(event.detail);
+            this.#list.splice(event.detail, 1);
+            console.log(this.#list);
         });
     }
     tablazatbaIr() {
-        this.#list.forEach((elem, index) => {
-            new MegjelenitSor(elem, this.tablaElem, index);
-        });
+        for (let index = 0; index < this.#list.length; index++) {
+            const elem = this.#list[index];
+
+            new MegjelenitSor(elem, this.tablaElem);
+        }
     }
 }
 export default Megjelenit;
--- MegjelenitSor.js
+++ MegjelenitSor.js
@@ -1,9 +1,9 @@
 class MegjelenitSor {
     #adat = {};
-    #index;
-    constructor(adat, szuloElem, index) {
+ 
+    constructor(adat, szuloElem) {
         this.#adat = adat;
-        this.#index = index;
+   
         this.tablaElem = szuloElem;
 
         this.#sor();
@@ -11,48 +11,28 @@ class MegjelenitSor {
         this.sorElem = this.tablaElem.children("tr:last-child");
         this.keszElem = this.sorElem.children("td").children(".kesz");
         this.torolElem = this.sorElem.children("td").children(".torol");
-        this.megseElem = this.sorElem.children("td").children(".megse");
-        /* this.megseElem.css("visibility", "hidden"); */
-        this.megseElem.css("display", "none");
+     
+      
         //console.log(this.keszElem);
         this.keszElem.on("click", () => {
-            this.sorElem.css("background-color", "lightgreen");
-            /*  this.megseElem.css("visibility", "visible");
-            this.keszElem.css("visibility", "hidden"); */
-            this.megseElem.css("display", "inline");
-            this.keszElem.css("display", "none");
+            console.log(this)
         });
-        this.megseElem.on("click", () => {
-            this.sorElem.css("background-color", "white");
-            /*  this.keszElem.css("visibility", "visible");
-            this.megseElem.css("visibility", "hidden"); */
-            this.keszElem.css("display", "inline");
-            this.megseElem.css("display", "none");
-        });
-        this.torolElem.on("click", () => {
-            //this.sorElem.remove();
-
-            this.#esemenyTrigger();
-        });
+       
+       
     }
     #sor() {
         let txt = "";
 
         txt += "<tr>";
         for (const key in this.#adat) {
-            if (Object.hasOwnProperty.call(this.#adat, key)) {
-                const element = this.#adat[key];
-                txt += `<td>${element}</td>`;
-            }
-        }
-        txt += `<td><span class="kesz">✔️</span> <span class="megse">❌</span><span class="torol">🗑</span></td>`;
+            txt += `<td>${this.#adat[key]}</td>`;
+          }
+        
+        txt += `<td><span class="kesz">✔️</span> <span class="torol">🗑</span></td>`;
         txt += "</tr>";
 
         this.tablaElem.append(txt);
     }
-    #esemenyTrigger() {
-        const esemenyem = new CustomEvent("torles", { detail: this.#index });
-        window.dispatchEvent(esemenyem);
-    }
+   
 }
 export default MegjelenitSor;
